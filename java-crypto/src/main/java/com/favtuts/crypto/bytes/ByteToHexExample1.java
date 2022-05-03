2d65cfe426439e1e8fc2c6a62cf10b3b56e89933
favtuts <favtuts@gmail.com>
2022-05-03 14:09:49 +0700

    convert bytes to hex using String.format %02x

---
+++ java-crypto/src/main/java/com/favtuts/crypto/bytes/ByteToHexExample1.java
@@ -0,0 +1,21 @@
+package com.favtuts.crypto.bytes;
+
+import java.nio.charset.StandardCharsets;
+
+public class ByteToHexExample1 {
+    
+    public static String hex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte aByte : bytes) {
+            result.append(String.format("%02x", aByte));
+            // upper case
+            // result.append(String.format("%02X", aByte));
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "a";
+        System.out.println(hex(input.getBytes(StandardCharsets.UTF_8)));
+    }
+}
