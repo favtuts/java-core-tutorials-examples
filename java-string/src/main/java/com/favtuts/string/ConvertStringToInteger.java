a6fb6c054111d88c8d479c1c9879711d7d3496fd
favtuts <favtuts@gmail.com>
2022-05-05 21:59:32 +0700

    Try and catch the NumberFormatException

--- java-string/src/main/java/com/favtuts/string/ConvertStringToInteger.java
+++ java-string/src/main/java/com/favtuts/string/ConvertStringToInteger.java
@@ -3,13 +3,23 @@ package com.favtuts.string;
 public class ConvertStringToInteger {
     public static void main(String[] args) {
 
+        /*
         String number = "99";
   
         // String to integer
         Integer result = Integer.valueOf(number);
   
         // 99
-        System.out.println(result);
+        System.out.println(result);*/
+        
+        String number = "D99";
+        try {
+            Integer result = Integer.valueOf(number);
+            System.out.println(result);
+        } catch (NumberFormatException e) {
+            //do something for the exception.
+            System.err.println("Invalid number format : " + number);
+        }
   
     }
 }
