94b2a7aaed9295ea115a225c9b219d5d304da908
favtuts <favtuts@gmail.com>
2022-05-05 21:42:00 +0700

    convert any numbers greater than Integer.MAX_VALUE will throw NumberFormatException

---
+++ java-string/src/main/java/com/favtuts/string/ConvertStringToInt2.java
@@ -0,0 +1,19 @@
+package com.favtuts.string;
+
+public class ConvertStringToInt2 {
+    
+    public static void main(String[] args) {
+        
+        // 2147483647
+        System.out.println(Integer.MAX_VALUE);
+
+        String number = "2147483648";
+        try {
+            int result = Integer.parseInt(number);
+            System.out.println(result);
+        } catch (Exception e) {
+            //do something for the exception.
+            System.err.println("Invalid number format : " + number);
+        }
+    }
+}
