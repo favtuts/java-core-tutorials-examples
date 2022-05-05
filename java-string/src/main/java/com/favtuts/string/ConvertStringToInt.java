25469164a153aed4262df9f94f04250e27ae3b63
favtuts <favtuts@gmail.com>
2022-05-05 20:23:32 +0700

    uses Integer.parseInt(String) to convert a String 1 to a primitive type int

---
+++ java-string/src/main/java/com/favtuts/string/ConvertStringToInt.java
@@ -0,0 +1,15 @@
+package com.favtuts.string;
+
+public class ConvertStringToInt {
+    public static void main(String[] args) {
+
+        String number = "1";
+  
+        // String to int
+        int result = Integer.parseInt(number);
+  
+        // 1
+        System.out.println(result);
+  
+    }
+}
