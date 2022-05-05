ba1640b3760eca2538aa06d0f6dd3346d910d87b
favtuts <favtuts@gmail.com>
2022-05-05 14:57:43 +0700

    use .equals to compare string with a null value, it throws NullPointerException

---
+++ java-string/src/main/java/com/favtuts/string/compare/StringCompareNull.java
@@ -0,0 +1,13 @@
+package com.favtuts.string.compare;
+
+public class StringCompareNull {
+    public static void main(String[] args) {
+
+        String str1 = null;
+
+        if (str1.equals("hello")) {
+            System.out.println("equals");
+        }
+
+    }
+}
