b4ce5796695e30edd32d5ffd0ed332a0fa302e7e
favtuts <favtuts@gmail.com>
2022-05-05 22:05:13 +0700

    Convert String to Integer with Java 8 Optional

---
+++ java-string/src/main/java/com/favtuts/string/ConvertStringToIntegerJava8.java
@@ -0,0 +1,38 @@
+package com.favtuts.string;
+
+import java.util.Optional;
+
+public class ConvertStringToIntegerJava8 {
+
+    public static void main(String[] args) {
+
+        String number = "99";
+
+        Optional<Integer> result = convertStringToInteger(number);
+
+        if (result.isPresent()) {
+            System.out.println(result.get());
+        } else {
+            System.err.println("Unable to convert the number : " + number);
+        }
+
+    }
+
+    private static Optional<Integer> convertStringToInteger(String input) {
+
+        if (input == null) return Optional.empty();
+
+        // remove spaces
+        input = input.trim();
+
+        if (input.isEmpty()) return Optional.empty();
+
+        try {
+            return Optional.of(Integer.valueOf(input));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+
+    }
+    
+}
