2a6ad09ed194d7cfdb4865dc9be88414fc4288d0
favtuts <favtuts@gmail.com>
2022-05-05 20:26:19 +0700

    handle NumberFormatException for invalid number

--- java-string/src/main/java/com/favtuts/string/ConvertStringToInt.java
+++ java-string/src/main/java/com/favtuts/string/ConvertStringToInt.java
@@ -3,13 +3,22 @@ package com.favtuts.string;
 public class ConvertStringToInt {
     public static void main(String[] args) {
 
+        /*
         String number = "1";
   
         // String to int
         int result = Integer.parseInt(number);
   
         // 1
-        System.out.println(result);
+        System.out.println(result);*/
   
+        String number = "1A";
+        try {
+            int result = Integer.parseInt(number);
+            System.out.println(result);
+        } catch (NumberFormatException e) {
+            //do something for the exception.
+            System.err.println("Invalid number format : " + number);
+        }
     }
 }
