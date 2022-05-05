1ac0caeebea0132978619faca6b21af37b4600f7
favtuts <favtuts@gmail.com>
2022-05-05 16:16:29 +0700

    test if the string contains delimiters before splitting the string

---
+++ java-string/src/main/java/com/favtuts/string/split/StringSplitContains.java
@@ -0,0 +1,19 @@
+package com.favtuts.string.split;
+
+public class StringSplitContains {
+    public static void main(String[] args) {
+
+        String phone = "012-3456789";
+  
+        if (phone.contains("-")) {
+  
+            String[] output = phone.split("-");
+            System.out.println(output[0]);  // 012
+            System.out.println(output[1]);  // 3456789
+  
+        } else {
+            throw new IllegalArgumentException("String " + phone + " does not contain -");
+        }
+  
+    }
+}
