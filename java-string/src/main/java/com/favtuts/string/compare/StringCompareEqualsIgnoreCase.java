6d776342de8894073ae1951b9c792ba67f54e8f2
favtuts <favtuts@gmail.com>
2022-05-05 14:53:44 +0700

    Using equalsIgnoreCase(), compare strings case-insensitive

---
+++ java-string/src/main/java/com/favtuts/string/compare/StringCompareEqualsIgnoreCase.java
@@ -0,0 +1,13 @@
+package com.favtuts.string.compare;
+
+public class StringCompareEqualsIgnoreCase {
+    public static void main(String[] args) {
+
+        String str1 = "apple";
+
+        boolean result = str1.equalsIgnoreCase("Apple");
+
+        System.out.println(result);    // true
+
+    }
+}
