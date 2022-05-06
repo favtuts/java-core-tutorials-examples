5f75fa537d263dcff0d9de56fd4f0cc38230b280
favtuts <favtuts@gmail.com>
2022-05-06 13:21:45 +0700

    use ByteArrayOutputStream to convert the InputStream to a String and print it out

--- java-string/README.md
+++ java-string/README.md
@@ -8,3 +8,5 @@
 * [How to convert String to Integer in Java](https://www.favtuts.com/how-to-convert-string-to-integer-in-java/)
 * [How to convert String to int in Java](https://www.favtuts.com/how-to-convert-string-to-int-in-java/)
 * [Java – How to check if a String is numeric](https://www.favtuts.com/java-how-to-check-if-a-string-is-numeric/)
+* [How to convert String to InputStream in Java](https://www.favtuts.com/how-to-convert-string-to-inputstream-in-java/)
+* [How to convert InputStream to String in Java](https://www.favtuts.com/how-to-convert-inputstream-to-string-in-java/)
---
+++ java-string/src/main/java/com/favtuts/string/ConvertInputStreamToString.java
@@ -0,0 +1,44 @@
+package com.favtuts.string;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class ConvertInputStreamToString {
+    
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    public static void main(String[] args) throws IOException {
+        
+        URI uri = URI.create("https://www.google.com/121");
+
+        try (InputStream inputStream = uri.toURL().openStream()) {
+            
+            // Convert InputStream -> String
+            String result = convertInputStreamToString(inputStream);
+
+            System.out.println(result);
+        }
+    }
+
+    // Plain Java
+    private static String convertInputStreamToString(InputStream is) throws IOException {
+
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+
+        // Java 1.1
+        //return result.toString(StandardCharsets.UTF_8.name());
+
+        return result.toString("UTF-8");
+
+        // Java 10
+        //return result.toString(StandardCharsets.UTF_8);
+
+    }
+}
