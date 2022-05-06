69ee1f8430d5a4265cc4d336b88a10614fb287aa
favtuts <favtuts@gmail.com>
2022-05-06 16:45:02 +0700

    create file with append mode

--- java-io/src/main/java/com/favtuts/io/file/CreateFileJava8.java
+++ java-io/src/main/java/com/favtuts/io/file/CreateFileJava8.java
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class CreateFileJava8 {
     
@@ -15,11 +16,21 @@ public class CreateFileJava8 {
 
         Path path = Paths.get(fileName);
 
+        /*
         // default, create, truncate and write to it.
         try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
             
             writer.write("Hello World !!");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        // Append mode.
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND)) {
+
+            writer.write("Hello World !!");
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
