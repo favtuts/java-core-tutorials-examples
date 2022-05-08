4b9e69cceb3fc829de110c68757479a79ab6a5b8
favtuts <favtuts@gmail.com>
2022-05-08 17:21:10 +0700

    uses File.find() to find files containing last modified time equals or after yesterday

--- java-io/src/main/java/com/favtuts/io/api/FileFindExample.java
+++ java-io/src/main/java/com/favtuts/io/api/FileFindExample.java
@@ -4,12 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileFindExample {
 
+    private static DateTimeFormatter DATE_FORMATTER
+            = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            
     public static void main(String[] args) throws IOException {
                 
         Path path = Paths.get("/home/tvt/workspace/favtuts");
@@ -18,11 +28,31 @@ public class FileFindExample {
         result.forEach(x -> System.out.println(x));
         */
 
+        /*
         long fileSize = 1024 * 1024 * 100; // 100M
 
         List<Path> result = findByFileSize(path, fileSize);
         for (Path p : result) {
             System.out.println(String.format("%-40s [fileSize]: %,d", p, Files.size(p)));
+        }
+        */
+
+        // find files, LastModifiedTime from yesterday and above
+        List<Path> result = findByLastModifiedTime(
+                //Paths.get("C:\\test"),
+                path,
+                Instant.now().minus(100, ChronoUnit.DAYS));
+
+        for (Path p : result) {
+
+            // formatting...
+            BasicFileAttributes attrs = Files.readAttributes(p, BasicFileAttributes.class);
+            FileTime time = attrs.lastModifiedTime();
+            LocalDateTime localDateTime = time.toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+
+            System.out.println(String.format("%-40s [%s]", p, localDateTime.format(DATE_FORMATTER)));
         }
     }
 
@@ -62,4 +92,31 @@ public class FileFindExample {
         return result;
     }
     
+
+    public static List<Path> findByLastModifiedTime(Path path, Instant instant)
+            throws IOException {
+
+        List<Path> result;
+        try (Stream<Path> pathStream = Files.find(path,
+                Integer.MAX_VALUE,
+                (p, basicFileAttributes) -> {
+
+                    if(Files.isDirectory(p) || !Files.isReadable(p)){
+                        return false;
+                    }
+
+                    FileTime fileTime = basicFileAttributes.lastModifiedTime();
+                    // negative if less, positive if greater
+                    // 1 means fileTime equals or after the provided instant argument
+                    // -1 means fileTime before the provided instant argument
+                    int i = fileTime.toInstant().compareTo(instant);
+                    return i > 0;
+                }
+
+        )) {
+            result = pathStream.collect(Collectors.toList());
+        }
+        return result;
+
+    }
 }
