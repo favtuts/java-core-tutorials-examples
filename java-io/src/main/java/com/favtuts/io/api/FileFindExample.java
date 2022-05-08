61dcf7f4716805dafdfce608ce3ef392717586d6
favtuts <favtuts@gmail.com>
2022-05-08 17:15:14 +0700

    uses Files.find() to find files containing file size greater or equals 100MB

--- java-io/src/main/java/com/favtuts/io/api/FileFindExample.java
+++ java-io/src/main/java/com/favtuts/io/api/FileFindExample.java
@@ -11,10 +11,19 @@ import java.util.stream.Stream;
 public class FileFindExample {
 
     public static void main(String[] args) throws IOException {
-        
+                
         Path path = Paths.get("/home/tvt/workspace/favtuts");
+        /*
         List<Path> result = findByFileName(path, "newFile.txt");
         result.forEach(x -> System.out.println(x));
+        */
+
+        long fileSize = 1024 * 1024 * 100; // 100M
+
+        List<Path> result = findByFileSize(path, fileSize);
+        for (Path p : result) {
+            System.out.println(String.format("%-40s [fileSize]: %,d", p, Files.size(p)));
+        }
     }
 
     public static List<Path> findByFileName(Path path, String fileName) throws IOException {
@@ -24,6 +33,29 @@ public class FileFindExample {
             path, 
             Integer.MAX_VALUE, 
             (p, basicFileAttributes) -> p.getFileName().toString().equalsIgnoreCase(fileName)
+        )) {
+            result = pathStream.collect(Collectors.toList());
+        }
+        return result;
+    }
+
+    public static List<Path> findByFileSize(Path path, Long fileSize) throws IOException {
+
+        List<Path> result;
+        try (Stream<Path> pathStream = Files.find(
+            path, 
+            Integer.MAX_VALUE, 
+            (p, basicFileAttributes) -> {
+                try {
+                    if (Files.isDirectory(p)) {   // ignore directory
+                        return false;
+                    }
+                    return Files.size(p) >= fileSize;
+                } catch (IOException e) {
+                    System.err.println("Unable to get the file size of path : " + path);
+                }
+                return false;
+            }
         )) {
             result = pathStream.collect(Collectors.toList());
         }
