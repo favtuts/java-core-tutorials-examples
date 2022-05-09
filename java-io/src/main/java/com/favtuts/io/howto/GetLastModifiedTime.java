7668d6765480a07e8b6c365168b00b8bcdcc3b3d
favtuts <favtuts@gmail.com>
2022-05-09 13:47:43 +0700

    displays the last modified time of a file in a custom date format

---
+++ java-io/src/main/java/com/favtuts/io/howto/GetLastModifiedTime.java
@@ -0,0 +1,61 @@
+package com.favtuts.io.howto;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class GetLastModifiedTime {
+    
+    private static final DateTimeFormatter DATE_FORMATTER = 
+        DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+    
+    public static void main(String[] args) {
+        
+        String fileName = "/home/tvt/workspace/favtuts/sample.csv";
+
+        try {
+            
+            Path file = Paths.get(fileName);
+            BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+
+            // default YYYY-MM-DDThh:mm:ss[.s+]Z
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());     // 2022-05-08T16:56:58.214937Z
+
+            FileTime lastModifiedTime = (FileTime) Files.getAttribute(file, "lastModifiedTime");
+            System.out.println("lastModifiedTime: " + lastModifiedTime);
+
+            System.out.println("creationTime: " + attr.creationTime());
+            System.out.println("lastAccessTime: " + attr.lastAccessTime());
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+
+            System.out.println("isDirectory: " + attr.isDirectory());
+            System.out.println("isOther: " + attr.isOther());
+            System.out.println("isRegularFile: " + attr.isRegularFile());
+            System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+            System.out.println("size: " + attr.size());
+
+            FileTime fileTime = attr.lastModifiedTime();
+            System.out.println("lastModifiedTime: " + formatDateTime(fileTime));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String formatDateTime(FileTime fileTime) {
+
+        LocalDateTime localDateTime = fileTime
+            .toInstant()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime();
+        
+        return localDateTime.format(DATE_FORMATTER);
+    }
+}
