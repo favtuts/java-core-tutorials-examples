10435411fd881c1a17a976be7fb16b08fb3efe06
favtuts <favtuts@gmail.com>
2022-05-09 10:11:27 +0700

    use String.contains() to check if a String contains a substring

---
+++ java-string/src/main/java/com/favtuts/string/StringAndSubstring.java
@@ -0,0 +1,30 @@
+package com.favtuts.string;
+
+public class StringAndSubstring {
+
+    public static void main(String[] args) {
+        
+        String name = "favtuts is learning Java 123";
+
+        System.out.println(name.contains("Java"));      // true
+        System.out.println(name.contains("java"));      // false
+        System.out.println(name.contains("FAVTUTS"));    // false
+        System.out.println(name.contains("favtuts"));    // true
+
+        if (name.contains("Java")) {
+            System.out.println("Hello Java");
+        } else {
+            System.out.println("abc...");
+        }
+
+        System.out.println(containsIgnoreCase(name, "Java"));      // true
+        System.out.println(containsIgnoreCase(name, "java"));      // true
+        System.out.println(containsIgnoreCase(name, "FAVTUTS"));    // true
+        System.out.println(containsIgnoreCase(name, "favtuts"));    // true
+    }
+
+    public static boolean containsIgnoreCase(String str, String subString) {
+        return str.toLowerCase().contains(subString.toLowerCase());
+    }
+    
+}
