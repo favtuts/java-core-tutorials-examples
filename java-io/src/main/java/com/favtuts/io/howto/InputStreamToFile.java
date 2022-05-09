ce4ec7e7be6ccb56786b51461df19147507d55fc
favtuts <favtuts@gmail.com>
2022-05-09 17:12:45 +0700

    convert InputStream to File in Java

--- java-io/src/main/java/com/favtuts/io/howto/InputStreamToFile.java
+++ java-io/src/main/java/com/favtuts/io/howto/InputStreamToFile.java
@@ -4,7 +4,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.apache.commons.io.FileUtils;
 
 public class InputStreamToFile {
 
@@ -21,6 +26,14 @@ public class InputStreamToFile {
             File file = new File("/home/tvt/workspace/favtuts/google.txt");
             copyInputStreamToFile(inputStream, file);
 
+            // commons-io
+            // FileUtils.copyInputStreamToFile(inputStream, file);
+
+            // Java 1.7
+            // Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            // Java 9
+            // copyInputStreamToFileJava9(inputStream, file);
         }
     }
     
@@ -35,6 +48,15 @@ public class InputStreamToFile {
             while((read = inputStream.read(bytes)) != -1) {
                 outputStream.write(bytes, 0, read);
             }
+        }
+    }
+
+    // Java 9
+    private static void copyInputStreamToFileJava9(InputStream input, File file) throws IOException {
+
+        // append = false
+        try (OutputStream output = new FileOutputStream(file, false)) {
+            input.transferTo(output);            
         }
     }
 
