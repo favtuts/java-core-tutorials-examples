e689c591cafb833c41cd233975ada4e5a2227b0d
favtuts <favtuts@gmail.com>
2022-05-10 10:06:21 +0700

    Send GET request Close with try-with-resources

---
+++ java-misc/src/main/java/com/favtuts/http/HttpClientExample1_2.java
@@ -0,0 +1,41 @@
+package com.favtuts.http;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpClientExample1_2 {
+
+    public static void main(String[] args) throws IOException {
+        
+        HttpGet request = new HttpGet("https://httpbin.org/get");
+        
+        // add request headers
+        request.addHeader("custom-key", "favtuts");
+        request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(request)) {
+            
+            // Get HttpResponse Status
+            System.out.println(response.getProtocolVersion());              // HTTP/1.1
+            System.out.println(response.getStatusLine().getStatusCode());   // 200
+            System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+            System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                // return it as a String
+                String result = EntityUtils.toString(entity);
+                System.out.println(result);
+            }
+        }
+    }
+    
+}
