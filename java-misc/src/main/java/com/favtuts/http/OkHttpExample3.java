69b1f1a7985f0a89950d14498d5f544bc7cd70f4
favtuts <favtuts@gmail.com>
2022-05-10 16:17:53 +0700

    OkHttpClient POST Request – Form Parameters

---
+++ java-misc/src/main/java/com/favtuts/http/OkHttpExample3.java
@@ -0,0 +1,44 @@
+package com.favtuts.http;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class OkHttpExample3 {
+
+    private final OkHttpClient httpClient = new OkHttpClient();
+    
+    public static void main(String[] args) throws IOException {
+        OkHttpExample3 obj = new OkHttpExample3();
+        obj.sendPOST();
+    }
+
+    private void sendPOST() throws IOException {
+
+        // form parameters
+        RequestBody formBody = new FormBody.Builder()
+                .add("username", "abc")
+                .add("password", "123")
+                .add("custom", "secret")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://httpbin.org/post")
+                .addHeader("User-Agent", "OkHttp Bot")
+                .post(formBody)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            // Get response body
+            System.out.println(response.body().string());
+        }
+
+    }
+}
