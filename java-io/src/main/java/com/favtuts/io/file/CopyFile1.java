799ab356a3c65389fb6590578a0c752201bca457
favtuts <favtuts@gmail.com>
2022-05-12 15:05:42 +0700

    the NIO Files.copy is the simplest way to copy a file

---
+++ java-io/src/main/java/com/favtuts/io/file/CopyFile1.java
@@ -0,0 +1,59 @@
+package com.favtuts.io.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CopyFile1 {
+
+    public static void main(String[] args) {
+        
+        String fromFile = "/home/tvt/workspace/favtuts/sample.csv";
+        String toFile = "/home/tvt/workspace/favtuts/backup/sample_bk_20220512.csv";
+
+        try {
+            copyFileNIO(fromFile, toFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Copy file is done.");
+
+    }
+    
+    public static void copyFileNIO(String from, String to) throws IOException {
+
+        Path fromFile = Paths.get(from);
+        Path toFile = Paths.get(to);
+
+        // if fromFile does't exist, Files.copy throws NoSuchFileException
+        if (Files.notExists(fromFile)) {
+            System.out.println("File doesn't exist? " + fromFile);
+            return;
+        }
+
+        // if toFile folder doesn't exist, Files.copy throws NoSuchFileException
+        // if toFile parent folder doesn't exist, create it.
+        Path parent = toFile.getParent();
+        if (parent!=null) {
+            if (Files.notExists(parent)) {
+                Files.createDirectories(parent);
+            }
+        }
+
+        // default - if toFile exist, throws FileAlreadyExistsException
+        Files.copy(fromFile, toFile);
+
+        // if toFile exist, replace it.
+        // Files.copy(fromFile, toFile, StandardCopyOption.REPLACE_EXISTING);
+
+        // multiple StandardCopyOption
+        /*CopyOption[] options = { StandardCopyOption.REPLACE_EXISTING,
+                StandardCopyOption.COPY_ATTRIBUTES,
+                LinkOption.NOFOLLOW_LINKS };
+
+        Files.copy(fromFile, toFile, options);*/
+    }
+    
+}
