532cdd6595674421b502468cd0074c8f68be15e5
favtuts <favtuts@gmail.com>
2022-05-12 15:26:41 +0700

    uses the plain Java InputStream and OutputStream to copy a file

--- java-io/src/main/java/com/favtuts/io/file/CopyFile1.java
+++ java-io/src/main/java/com/favtuts/io/file/CopyFile1.java
@@ -1,7 +1,11 @@
 package com.favtuts.io.file;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,7 +22,8 @@ public class CopyFile1 {
         try {
             //copyFileNIO(fromFile, toFile);
             //copyFileCommonIO(fromFile, toFile);
-            copyFileGuava(fromFile, toFile);
+            //copyFileGuava(fromFile, toFile);
+            copyFilePlainJava(fromFile, toFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,6 +82,37 @@ public class CopyFile1 {
   
         // @Beta?
         com.google.common.io.Files.copy(fromFile, toFile);
+  
+    }
+
+    public static void copyFilePlainJava(String from, String to) throws IOException {
+
+        InputStream inStream = null;
+        OutputStream outStream = null;
+  
+        try {
+  
+            File fromFile = new File(from);
+            File toFile = new File(to);
+  
+            inStream = new FileInputStream(fromFile);
+            outStream = new FileOutputStream(toFile);
+  
+            byte[] buffer = new byte[1024];
+  
+            int length;
+            while ((length = inStream.read(buffer)) > 0) {
+                outStream.write(buffer, 0, length);
+                outStream.flush();
+            }
+  
+        } finally {
+            if (inStream != null)
+                inStream.close();
+  
+            if (outStream != null)
+                outStream.close();
+        }
   
     }
 }
