607ee78cd456381013ccf03107785ee347f905cf
favtuts <favtuts@gmail.com>
2022-05-12 14:29:41 +0700

    use HttpServletRequest.getRemoteAddr() to get the client IP address

---
+++ java-misc/src/main/java/com/favtuts/http/WebUtils.java
@@ -0,0 +1,28 @@
+package com.favtuts.http;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class WebUtils {
+    
+    private Map<String, String> getHeadersInfo(HttpServletRequest request) {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    private String getUserAgent(HttpServletRequest request) {
+        return request.getHeader("user-agent");
+    }
+}
