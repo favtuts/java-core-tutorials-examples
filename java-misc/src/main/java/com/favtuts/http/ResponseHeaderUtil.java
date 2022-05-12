2a337bdad3f000c506d0c23ed18771abd69ea097
favtuts <favtuts@gmail.com>
2022-05-12 13:07:12 +0700

    get response headers value via URLConnection

---
+++ java-misc/src/main/java/com/favtuts/http/ResponseHeaderUtil.java
@@ -0,0 +1,42 @@
+package com.favtuts.http;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class ResponseHeaderUtil {
+
+    public static void main(String[] args) {
+
+        try {
+
+            URL obj = new URL("http://favtuts.com");
+            URLConnection conn = obj.openConnection();
+            Map<String, List<String>> map = conn.getHeaderFields();
+
+            System.out.println("Printing Response Header...\n");
+
+            for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                System.out.println("Key : " + entry.getKey()
+                        + " ,Value : " + entry.getValue());
+            }
+
+            System.out.println("\nGet Response Header By Key ...\n");
+            String server = conn.getHeaderField("Server");
+
+            if (server == null) {
+                System.out.println("Key 'Server' is not found!");
+            } else {
+                System.out.println("Server - " + server);
+            }
+
+            System.out.println("\n Done");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
