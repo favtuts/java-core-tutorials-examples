6ae6df3cf7baaf5bd9040c3853d3e25b4b55901d
favtuts <favtuts@gmail.com>
2022-05-14 14:22:31 +0700

    sort a map by keys use LinkedHashMap to keep the order

---
+++ java-basic/README.md
@@ -0,0 +1,3 @@
+# Java Core Tutorials
+
+* [Java 8 – How to sort a Map](https://www.favtuts.com/java-8-how-to-sort-a-map/)
--- java-misc/src/main/java/com/favtuts/display/DisplayApp.java
+++ java-misc/src/main/java/com/favtuts/display/DisplayApp.java
@@ -1,12 +1,34 @@
 package com.favtuts.display;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class DisplayApp {
 
     public static void main(String[] args) {
         
+        //listSystemProperties();
+        sortSystemProperties();
+        
+    }
+
+    private static void sortSystemProperties () {
+        Properties properties = System.getProperties();
+
+        // Thanks Java 8
+        LinkedHashMap<String, String> collect = properties.entrySet().stream()
+                .collect(Collectors.toMap(k -> (String) k.getKey(), e -> (String) e.getValue()))
+                .entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        
+        collect.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    private static void listSystemProperties () {
+
         Properties properties = System.getProperties();
 
         // Java 8
@@ -19,6 +41,7 @@ public class DisplayApp {
 
         // No good, output is truncated, long lines end with ...
         // properties.list(System.out);
+
     }
     
 }
