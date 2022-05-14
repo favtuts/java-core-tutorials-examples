eee890627b9c127a32fd37f075b1468c615fd5f3
favtuts <favtuts@gmail.com>
2022-05-14 15:43:16 +0700

    Upgrade the sortByValue() method to support generics

--- java-basic/src/main/java/com/favtuts/classic/SortMapExamples.java
+++ java-basic/src/main/java/com/favtuts/classic/SortMapExamples.java
@@ -38,8 +38,29 @@ public class SortMapExamples {
         printMap(unsortMap);
 
         System.out.println("\nSorted Map......By Value");
-        Map<String, Integer> sortedMap = sortByValue(unsortMap);
+        //Map<String, Integer> sortedMap = sortByValue(unsortMap);
+        Map<String, Integer> sortedMap = sortByValueGeneric(unsortMap);
         printMap(sortedMap);
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValueGeneric(Map<K, V> unsortMap) {
+
+        List<Map.Entry<K, V>> list =
+                new LinkedList<Map.Entry<K, V>>(unsortMap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+
     }
 
     private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
