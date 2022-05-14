a389af42891d63e64e41a10cdd48b352715de230
favtuts <favtuts@gmail.com>
2022-05-14 15:38:49 +0700

    sort map by value with List<Map> and custom Comparator and put it into order map LinkedHashMap

--- java-basic/src/main/java/com/favtuts/classic/SortMapExamples.java
+++ java-basic/src/main/java/com/favtuts/classic/SortMapExamples.java
@@ -1,7 +1,12 @@
 package com.favtuts.classic;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -10,8 +15,62 @@ public class SortMapExamples {
     public static void main(String[] args) {
         
         //sortByKeyAscWithTreeMap();
-        sortByKeyDescWithTreeMap();
+        //sortByKeyDescWithTreeMap();
+        sortByValueWithListMap();
 
+    }
+
+    private static void sortByValueWithListMap() {
+        Map<String, Integer> unsortMap = new HashMap<String, Integer>();
+        unsortMap.put("z", 10);
+        unsortMap.put("b", 5);
+        unsortMap.put("a", 6);
+        unsortMap.put("c", 20);
+        unsortMap.put("d", 1);
+        unsortMap.put("e", 7);
+        unsortMap.put("y", 8);
+        unsortMap.put("n", 99);
+        unsortMap.put("j", 50);
+        unsortMap.put("m", 2);
+        unsortMap.put("f", 9);
+
+        System.out.println("Unsort Map......");
+        printMap(unsortMap);
+
+        System.out.println("\nSorted Map......By Value");
+        Map<String, Integer> sortedMap = sortByValue(unsortMap);
+        printMap(sortedMap);
+    }
+
+    private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+
+        // 1. Convert Map to List of Map
+        List<Map.Entry<String, Integer>> list = 
+            new LinkedList<Map.Entry<String, Integer>> (unsortMap.entrySet());
+        
+        // 2. Sort list with Collections.sort(), provide a custom Comparator
+        // Try swith the o1 o2 posistion for a different order
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        /*
+        //classic iterator example
+        for (Iterator<Map.Entry<String, Integer>> it = list.iterator(); it.hasNext(); ) {
+            Map.Entry<String, Integer> entry = it.next();
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        */
+
+        return sortedMap;
     }
 
     private static void sortByKeyDescWithTreeMap() {
