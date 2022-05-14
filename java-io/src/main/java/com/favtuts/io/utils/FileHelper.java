7d789aed2f0c181d16d28280760215004dcc6db5
favtuts <favtuts@gmail.com>
2022-05-14 23:15:43 +0700

    In static method, update getClass() to ClassName.class

--- java-io/src/main/java/com/favtuts/io/utils/FileHelper.java
+++ java-io/src/main/java/com/favtuts/io/utils/FileHelper.java
@@ -1,13 +1,19 @@
 package com.favtuts.io.utils;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class FileHelper {
 
     public static void main(String[] args) {
+        
+        // Non Static Method
         FileHelper obj = new FileHelper();
         System.out.println(obj.getFilePathToSave());
+
+        // Static Method
+        System.out.println(getFilePathToSaveStatic());
     }
 
     public String getFilePathToSave() {
@@ -20,7 +26,24 @@ public class FileHelper {
         ) {
             prop.load(inputStream);
             result = prop.getProperty("json.filepath");
-        } catch (Exception e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static String getFilePathToSaveStatic() {
+
+        Properties prop = new Properties();        
+        String result = "";
+
+        try (InputStream inputStream = FileHelper.class.getClassLoader()
+                                            .getResourceAsStream("config.properties")
+        ) {
+            prop.load(inputStream);
+            result = prop.getProperty("json.filepath");
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
