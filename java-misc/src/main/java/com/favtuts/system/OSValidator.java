1a561996360da8208f79618b36826fc91b479598
favtuts <favtuts@gmail.com>
2022-05-14 16:36:23 +0700

    increase the performance by using static fields to ensures the OS.indexOf checking runs once only

--- java-misc/src/main/java/com/favtuts/system/OSValidator.java
+++ java-misc/src/main/java/com/favtuts/system/OSValidator.java
@@ -2,11 +2,17 @@ package com.favtuts.system;
 
 public class OSValidator {
     
-    private static String OS = System.getProperty("os.name").toLowerCase();    
+    private static String OS = System.getProperty("os.name").toLowerCase(); 
+    public static boolean IS_WINDOWS = (OS.indexOf("win") >= 0);
+    public static boolean IS_MAC = (OS.indexOf("mac") >= 0);
+    public static boolean IS_UNIX = (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+    public static boolean IS_SOLARIS = (OS.indexOf("sunos") >= 0);
     public static void main(String[] args) {
         
         System.out.println("os.name: " + OS);
 
+        // Original Version
+        /*
         if (isWindows()) {
             System.out.println("This is Windows");
         } else if (isMac()) {
@@ -14,6 +20,20 @@ public class OSValidator {
         } else if (isUnix()) {
             System.out.println("This is Unix or Linux");
         } else if (isSolaris()) {
+            System.out.println("This is Solaris");
+        } else {
+            System.out.println("Your OS is not support!!");
+        }
+        */
+
+        // Enhanced Version
+        if (IS_WINDOWS) {
+            System.out.println("This is Windows");
+        } else if (IS_MAC) {
+            System.out.println("This is Mac");
+        } else if (IS_UNIX) {
+            System.out.println("This is Unix or Linux");
+        } else if (IS_SOLARIS) {
             System.out.println("This is Solaris");
         } else {
             System.out.println("Your OS is not support!!");
