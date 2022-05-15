8253c0b629427f3940a658eb7dcfee4186a67e8d
favtuts <favtuts@gmail.com>
2022-05-15 21:46:51 +0700

    use fis.available() to check the remaining bytes that can be read

--- java-io/src/main/java/com/favtuts/io/api/inputstream/FileInputStreamExample.java
+++ java-io/src/main/java/com/favtuts/io/api/inputstream/FileInputStreamExample.java
@@ -7,12 +7,32 @@ import java.io.IOException;
 public class FileInputStreamExample {
 
     public static void main(String[] args) {
-        String file = "/home/tvt/workspace/favtuts/writefile.txt";
+        String file = "/home/tvt/workspace/favtuts/test.txt";
 
-        readFile(file);
+        //readFileExample1(file);
+        readFileExample2(file);
     }
     
-    private static void readFile(String fileName) {
+    private static void readFileExample2(String fileName) {
+
+        try (FileInputStream fis = new FileInputStream(new File(fileName))) {
+
+            // remain bytes that can be read
+            System.out.println("Remaining bytes that can be read : " + fis.available());
+
+            int content;
+            // reads a byte at a time, if it reached end of the file, returns -1
+            while((content = fis.read()) != -1) {
+                System.out.println((char)content);
+
+                System.out.println("Remaining bytes that can be read : " + fis.available());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readFileExample1(String fileName) {
 
         try (FileInputStream fis = new FileInputStream(new File(fileName))) {
             int content;
