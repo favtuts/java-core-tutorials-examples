6b4e1f0952d677c4414b34db11766e42eb504a81
favtuts <favtuts@gmail.com>
2022-05-15 22:12:10 +0700

    use InputStreamReader to convert a FileInputStream to BufferedReader, and read it line by line

--- java-io/src/main/java/com/favtuts/io/api/inputstream/FileInputStreamExample.java
+++ java-io/src/main/java/com/favtuts/io/api/inputstream/FileInputStreamExample.java
@@ -1,9 +1,11 @@
 package com.favtuts.io.api.inputstream;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 public class FileInputStreamExample {
@@ -14,7 +16,26 @@ public class FileInputStreamExample {
         // readFileExample1(fileName);
         // readFileExample2(fileName);
         // readFileBetterPerformance(fileName);
-        readFileBetterPerformance2(fileName);
+        // readFileBetterPerformance2(fileName);
+        readFileBetterInputStreamReader(fileName);
+    }
+
+    private static void readFileBetterInputStreamReader(String fileName) {
+
+        try (BufferedReader br =
+                     new BufferedReader(
+                             new InputStreamReader(
+                                     new FileInputStream(new File(fileName))))) {
+    
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
     }
 
     private static void readFileBetterPerformance2(String fileName) {
