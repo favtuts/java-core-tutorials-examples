69c2e0037c5cb1c474f6923c5e614c60daa32b87
favtuts <favtuts@gmail.com>
2022-05-16 15:46:06 +0700

    Before the JDK 7 try-resources, we need to handle the close() manually

--- java-io/src/main/java/com/favtuts/io/file/WriteFileBufferedWriter.java
+++ java-io/src/main/java/com/favtuts/io/file/WriteFileBufferedWriter.java
@@ -8,6 +8,40 @@ public class WriteFileBufferedWriter {
 
     public static void main(String[] args) {
         
+        //writeFileBufferedWriterJava7();
+        writeFileBufferedWriterClassic();
+    }
+
+    private static void writeFileBufferedWriterClassic() {
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try {
+
+            String content = "This is the content to write into file\n";
+
+            fw = new FileWriter("app.log");
+            //fw = new FileWriter("app.log", true);  Append mode
+            bw = new BufferedWriter(fw);
+            bw.write(content);
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+            } catch (IOException ex) {
+                System.err.format("IOException: %s%n", ex);
+            }
+        }
+    }
+    
+
+    private static void writeFileBufferedWriterJava7(){
         String content = "This is the content to write into file\n";
 
         // If the file doesn't exists, create and write to it
@@ -22,5 +56,4 @@ public class WriteFileBufferedWriter {
             System.err.format("IOException: %s%n", e);
         }
     }
-    
 }
