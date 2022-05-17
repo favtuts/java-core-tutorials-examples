4543c332d580c6fa732ed9889c7b0e5b5cb1946e
favtuts <favtuts@gmail.com>
2022-05-17 15:17:16 +0700

    The extra checking ensures the last file extension index (position) is always after the last file separator (Windows or Unix)

--- java-io/src/main/java/com/favtuts/io/howto/GetFileExtension.java
+++ java-io/src/main/java/com/favtuts/io/howto/GetFileExtension.java
@@ -3,6 +3,9 @@ package com.favtuts.io.howto;
 public class GetFileExtension {
 
     private static final String OUTPUT_FORMAT = "Path: %-30s -> File Extension: %s";
+    private static final String WINDOWS_FILE_SEPARATOR = "\\";
+    private static final String UNIX_FILE_SEPARATOR = "/";
+    private static final String FILE_EXTENSION = ".";
 
     public static void main(String[] args) {
         
@@ -19,9 +22,40 @@ public class GetFileExtension {
         };
 
         for (String file : files) {
-            String output = String.format(OUTPUT_FORMAT, file, getFileExtension(file));
+            //String output = String.format(OUTPUT_FORMAT, file, getFileExtension(file));
+            String output = String.format(OUTPUT_FORMAT, file, getFileExtensionImproved(file));
             System.out.println(output);
         }
+    }
+
+    /**
+     * Add extra checking for below cases
+     * <p>
+     * "/path/../makefile",
+     * "/path/dir.test/makefile"
+     */
+    public static String getFileExtensionImproved(String fileName) {
+        if (fileName == null) {
+            throw new IllegalArgumentException("fileName must not be null!");
+        }
+
+        String extension = "";
+
+        int indexOfLastExtension = fileName.lastIndexOf(FILE_EXTENSION);
+
+        // check last file separator, windows and unix
+        int lastSeparatorPosWindows = fileName.lastIndexOf(WINDOWS_FILE_SEPARATOR);
+        int lastSeparatorPosUnix = fileName.lastIndexOf(UNIX_FILE_SEPARATOR);
+
+        // takes the greater of the two values, which mean last file separator
+        int indexOflastSeparator = Math.max(lastSeparatorPosWindows, lastSeparatorPosUnix);
+
+        // make sure the file extension appear after the last file separator
+        if (indexOfLastExtension > indexOflastSeparator) {
+            extension = fileName.substring(indexOfLastExtension + 1);
+        }
+
+        return extension;
     }
 
 
