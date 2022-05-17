6b598815563de1ad403498a9e8744b3de75071df
favtuts <favtuts@gmail.com>
2022-05-17 17:22:24 +0700

    uses FileUtils.copyDirectory Apache commons-io to copy a directory and its content, a friendly, and straightforward API

--- java-io/src/main/java/com/favtuts/io/howto/CopyDirectory.java
+++ java-io/src/main/java/com/favtuts/io/howto/CopyDirectory.java
@@ -1,21 +1,25 @@
 package com.favtuts.io.howto;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import com.favtuts.io.utils.TreeCopyFileVisitor;
 
+import org.apache.commons.io.FileUtils;
+
 public class CopyDirectory {
-    
+
     public static void main(String[] args) {
         String fromDirectory = "/home/tvt/workspace/favtuts/test/";
         String toToDirectory = "/home/tvt/workspace/favtuts/test2/";
 
         try {
 
-            copyDirectoryFileVisitor(fromDirectory, toToDirectory);
-        
+            //copyDirectoryFileVisitor(fromDirectory, toToDirectory);
+            copyFileCommonIO(fromDirectory, toToDirectory);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -23,11 +27,18 @@ public class CopyDirectory {
         System.out.println("Done");
     }
 
-
     public static void copyDirectoryFileVisitor(String source, String target) throws IOException {
 
         TreeCopyFileVisitor fileVisitor = new TreeCopyFileVisitor(source, target);
         Files.walkFileTree(Paths.get(source), fileVisitor);
     }
 
+    public static void copyFileCommonIO(String from, String to) throws IOException {
+
+        File fromDir = new File(from);
+        File toDir = new File(to);
+
+        FileUtils.copyDirectory(fromDir, toDir);
+
+    }
 }
