88e2e5f90b9c4dc9abfe7d0a51c6bfbbfea1afa7
favtuts <favtuts@gmail.com>
2022-05-17 15:25:29 +0700

    hardcode some file extensions and display the predefined result

--- java-io/src/main/java/com/favtuts/io/howto/GetFileExtension.java
+++ java-io/src/main/java/com/favtuts/io/howto/GetFileExtension.java
@@ -1,11 +1,16 @@
 package com.favtuts.io.howto;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class GetFileExtension {
 
     private static final String OUTPUT_FORMAT = "Path: %-30s -> File Extension: %s";
     private static final String WINDOWS_FILE_SEPARATOR = "\\";
     private static final String UNIX_FILE_SEPARATOR = "/";
     private static final String FILE_EXTENSION = ".";
+    private static final Map<String, String> KNOWN_EXTENSION = createKnownExtensionMap();
 
     public static void main(String[] args) {
         
@@ -23,7 +28,8 @@ public class GetFileExtension {
 
         for (String file : files) {
             //String output = String.format(OUTPUT_FORMAT, file, getFileExtension(file));
-            String output = String.format(OUTPUT_FORMAT, file, getFileExtensionImproved(file));
+            //String output = String.format(OUTPUT_FORMAT, file, getFileExtensionImproved(file));
+            String output = String.format(OUTPUT_FORMAT, file, getFileExtensionKnownExtension(file));
             System.out.println(output);
         }
     }
@@ -78,6 +84,38 @@ public class GetFileExtension {
         }
 
         return extension;
+    }
+
+    // hardcoded
+    public static String getFileExtensionKnownExtension(final String fileName) {
+
+        if (fileName == null) {
+            throw new IllegalArgumentException("fileName must not be null!");
+        }
+
+        // if the file name is end with the hard coded extension.
+        // Java 8 stream, loop map if key matches get value
+        String extension = KNOWN_EXTENSION
+                .entrySet()
+                .stream()
+                .filter(x -> fileName.endsWith(x.getKey()))
+                .map(x -> x.getValue())
+                .collect(Collectors.joining());
+
+        if ("".equals(extension)) {
+            extension = getFileExtensionImproved(fileName); // see example 2
+        }
+
+        return extension;
+
+    }
+
+    private static Map<String, String> createKnownExtensionMap() {
+        Map<String, String> result = new HashMap<>();
+        result.put(".tar.gz", "tar.gz");    // if .tar.gz, gets tar.gz
+        result.put("makefile", "make");     // if makefile, get make
+        //...extra
+        return result;
     }
     
 }
