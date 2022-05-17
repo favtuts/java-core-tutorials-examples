c8d1bbf94cde77a94cbbd33385fa9751573243e4
favtuts <favtuts@gmail.com>
2022-05-17 17:35:06 +0700

    uses Java 8 Files.list to simulate a file walker and Java 7 NIO Files to check, create, and copy the directory and its content

--- java-io/src/main/java/com/favtuts/io/howto/CopyDirectory.java
+++ java-io/src/main/java/com/favtuts/io/howto/CopyDirectory.java
@@ -3,7 +3,10 @@ package com.favtuts.io.howto;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
 
 import com.favtuts.io.utils.TreeCopyFileVisitor;
 
@@ -18,7 +21,8 @@ public class CopyDirectory {
         try {
 
             //copyDirectoryFileVisitor(fromDirectory, toToDirectory);
-            copyFileCommonIO(fromDirectory, toToDirectory);
+            //copyFileCommonIO(fromDirectory, toToDirectory);
+            copyDirectoryJavaNIO(Paths.get(fromDirectory),Paths.get(toToDirectory));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,6 +43,50 @@ public class CopyDirectory {
         File toDir = new File(to);
 
         FileUtils.copyDirectory(fromDir, toDir);
+
+    }
+
+    public static void copyDirectoryJavaNIO(Path source, Path target) throws IOException {
+
+        // is this a directory?
+        if (Files.isDirectory(source)) {
+
+            //if target directory exist?
+            if (Files.notExists(target)) {
+                // create it
+                Files.createDirectories(target);
+                System.out.println("Directory created : " + target);
+            }
+
+            // list all files or folders from the source, Java 1.8, returns a stream
+            // doc said need try-with-resources, auto-close stream
+            try (Stream<Path> paths = Files.list(source)) {
+
+                // recursive loop
+                paths.forEach(p ->
+                        copyDirectoryJavaNIOWrapper(
+                            p, target.resolve(source.relativize(p)))
+                );
+
+            }
+
+        } else {
+            // if file exists, replace it
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println(
+                    String.format("Copy File from \t'%s' to \t'%s'", source, target)
+            );
+        }    
+    }
+
+    // extract method to handle exception in lambda
+    public static void copyDirectoryJavaNIOWrapper(Path source, Path target) {
+
+        try {
+            copyDirectoryJavaNIO(source, target);
+        } catch (IOException e) {
+            System.err.println("IO errors : " + e.getMessage());
+        }
 
     }
 }
