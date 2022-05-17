000f60075b13e73b47ea63a15c03db7bbed7c7b4
favtuts <favtuts@gmail.com>
2022-05-17 10:26:32 +0700

    The Files.deleteIfExists(Path) returns true if file deletion success false if the file doesn’t exist, no exception is thrown

--- java-io/src/main/java/com/favtuts/io/file/DeleteFile.java
+++ java-io/src/main/java/com/favtuts/io/file/DeleteFile.java
@@ -9,11 +9,28 @@ public class DeleteFile {
     public static void main(String[] args) {
         
         String fileName = "/home/tvt/workspace/favtuts/test.txt";
+        // deleteFileHasException(fileName);
+        deleteFileNoException(fileName);
+    }
+    
+    private static void deleteFileNoException(String fileName) {
+        try {
+            boolean result = Files.deleteIfExists(Paths.get(fileName));
+            if (result) {
+                System.out.println("File is deleted!");
+            } else {
+                System.out.println("Sorry, unable to delete the file.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteFileHasException(String fileName) {
         try {
             Files.delete(Paths.get(fileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
 }
