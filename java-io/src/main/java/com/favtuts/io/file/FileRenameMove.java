bd93940abb3bd58ab52e686e78f07587204d9cfb
favtuts <favtuts@gmail.com>
2022-05-18 13:48:11 +0700

    rename or move a file in Java

--- java-io/src/main/java/com/favtuts/io/file/FileRenameMove.java
+++ java-io/src/main/java/com/favtuts/io/file/FileRenameMove.java
@@ -1,10 +1,13 @@
 package com.favtuts.io.file;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+
+import org.apache.commons.io.FileUtils;
 
 public class FileRenameMove {
 
@@ -27,6 +30,24 @@ public class FileRenameMove {
 
             // rename a file in the same directory
             // Files.move(source, source.resolveSibling("newName.txt"));
+
+            // move file to another directory, using the same file name
+            /*
+            Path newDir = Paths.get("/home/tvt/workspace/favtuts/newfolder/");
+            // create the directories, if directory exits, no effect
+            Files.createDirectories(newDir);
+
+            Files.move(source, newDir.resolve(source.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+            */
+
+            // apache commons-io
+            //FileUtils.moveFile(new File("/source"), new File("/target"));
+
+            /*CopyOption[] options = { StandardCopyOption.REPLACE_EXISTING,
+                    StandardCopyOption.COPY_ATTRIBUTES,
+                    LinkOption.NOFOLLOW_LINKS };
+
+            Files.move(source, target, options);*/
 
         } catch (IOException e) {
             e.printStackTrace();
