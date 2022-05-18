94ecbd0814328770b7cc6caeec65273694604860
favtuts <favtuts@gmail.com>
2022-05-18 16:20:25 +0700

    use the legacy IO File.length() to get the size of a file in bytes

--- java-io/src/main/java/com/favtuts/io/howto/GetFileSize.java
+++ java-io/src/main/java/com/favtuts/io/howto/GetFileSize.java
@@ -1,5 +1,6 @@
 package com.favtuts.io.howto;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,7 +11,8 @@ public class GetFileSize {
     public static void main(String[] args) {
         // this image is around 140kb
         String fileName = "/home/tvt/Pictures/EOM.png";
-        printFileSizeNIO(fileName);
+        //printFileSizeNIO(fileName);
+        printFileSize(fileName);
     }
 
     public static void printFileSizeNIO(String fileName) {
@@ -26,6 +28,40 @@ public class GetFileSize {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    public static void printFileSize(String fileName) {
+
+        File file = new File(fileName);
+
+        if (file.exists()) {
+
+            // size of a file (in bytes)
+            long bytes = file.length();
+
+            long kilobytes = (bytes / 1024);
+            long megabytes = (kilobytes / 1024);
+            long gigabytes = (megabytes / 1024);
+            long terabytes = (gigabytes / 1024);
+            long petabytes = (terabytes / 1024);
+            long exabytes = (petabytes / 1024);
+            long zettabytes = (exabytes / 1024);
+            long yottabytes = (zettabytes / 1024);
+
+            System.out.println(String.format("%,d bytes", bytes));
+            System.out.println(String.format("%,d kilobytes", kilobytes));
+            System.out.println(String.format("%,d megabytes", megabytes));
+            System.out.println(String.format("%,d gigabytes", gigabytes));
+            System.out.println(String.format("%,d terabytes", terabytes));
+            System.out.println(String.format("%,d petabytes", petabytes));
+            System.out.println(String.format("%,d exabytes", exabytes));
+            System.out.println(String.format("%,d zettabytes", zettabytes));
+            System.out.println(String.format("%,d yottabytes", yottabytes));
+
+        } else {
+            System.out.println("File does not exist!");
         }
 
     }
