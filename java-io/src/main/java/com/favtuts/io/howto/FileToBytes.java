4ad48e2364d2c313d4c01364ef3d4ffe89ed146e
favtuts <favtuts@gmail.com>
2022-05-19 10:10:58 +0700

    add few ways to read file into bytes

--- java-io/src/main/java/com/favtuts/io/howto/FileToBytes.java
+++ java-io/src/main/java/com/favtuts/io/howto/FileToBytes.java
@@ -1,9 +1,13 @@
 package com.favtuts.io.howto;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
 
 public class FileToBytes {
     
@@ -23,5 +27,49 @@ public class FileToBytes {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    // Apache Commons IO, try FileUtils.
+    private static void readFileToBytesCommonsIO(String filePath) throws IOException {
+        //...
+        File file = new File("/path/file");
+        byte[] bytes = FileUtils.readFileToByteArray(file);
+    }
+
+
+    // try-with-resources version
+    private static void readFileToBytesJava7(String filePath) throws IOException {
+
+        File file = new File(filePath);
+        byte[] bytes = new byte[(int) file.length()];
+    
+        // funny, if can use Java 7, please uses Files.readAllBytes(path)
+        try(FileInputStream fis = new FileInputStream(file)){
+            fis.read(bytes);
+        }
+    
+    }
+
+    // file to byte[], old and classic way, before Java 7
+    private static void readFileToBytesClassic(String filePath) throws IOException {
+
+        File file = new File(filePath);
+        byte[] bytes = new byte[(int) file.length()];
+  
+        FileInputStream fis = null;
+        try {
+  
+            fis = new FileInputStream(file);
+  
+            //read file into bytes[]
+            fis.read(bytes);
+  
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+  
     }
 }
