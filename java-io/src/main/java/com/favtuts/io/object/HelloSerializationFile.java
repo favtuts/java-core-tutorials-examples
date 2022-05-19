479e430c1376e49acffd7471bff5a70d8fa8f98e
favtuts <favtuts@gmail.com>
2022-05-19 15:33:29 +0700

    More Serialization examples

--- java-io/src/main/java/com/favtuts/io/object/HelloSerializationFile.java
+++ java-io/src/main/java/com/favtuts/io/object/HelloSerializationFile.java
@@ -1,11 +1,13 @@
 package com.favtuts.io.object;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 
 public class HelloSerializationFile {
@@ -21,7 +23,7 @@ public class HelloSerializationFile {
         Person p = readObjectFromFile(file);
 
         System.out.println(p);        
-        
+
     }
 
     // Serialization
@@ -35,6 +37,28 @@ public class HelloSerializationFile {
         }
     }
 
+    // Serialization
+    // Convert object to OutputStream
+    public static void writeObjectToStream(Object obj, OutputStream output) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(output)) {
+            oos.writeObject(obj);
+            oos.flush();
+        }
+    }
+
+    // Serialization
+    // Convert object to byte[]
+    public static byte[] writeObjectToStream(Object obj) {
+        ByteArrayOutputStream boas = new ByteArrayOutputStream();
+        try (ObjectOutputStream ois = new ObjectOutputStream(boas)) {
+            ois.writeObject(obj);
+            return boas.toByteArray();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+
     // Deserialization
     // Get object from a file
     public static Person readObjectFromFile(File file) throws IOException, ClassNotFoundException {
@@ -43,6 +67,17 @@ public class HelloSerializationFile {
             ObjectInputStream ois = new ObjectInputStream(fis)
         ) {
             result = (Person) ois.readObject();
+        }
+        return result;
+    }
+
+    // Deserialization
+    // Get object from a file.
+    public static Object readObjectFromFile2(File file) throws IOException, ClassNotFoundException {
+        Object result = null;
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            result = ois.readObject();
         }
         return result;
     }
