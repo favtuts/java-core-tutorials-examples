0aa85b678547cfa128dd15b7e19420b472e5a10e
favtuts <favtuts@gmail.com>
2022-05-19 09:27:46 +0700

    List available of MessageDigest algorithms

--- java-crypto/src/main/java/com/favtuts/crypto/hash/FileChecksum.java
+++ java-crypto/src/main/java/com/favtuts/crypto/hash/FileChecksum.java
@@ -7,32 +7,44 @@ import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 public class FileChecksum {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         
-        //MessageDigest md = MessageDigest.getInstance("SHA-256"); // SHA, MD2, MD5, SHA-256, SHA-384...
-        MessageDigest md = MessageDigest.getInstance("MD5"); 
-        String hex = checksum("/home/tvt/workspace/favtuts/server.log", md);
-        System.out.println(hex);
+        String filePath = "/home/tvt/workspace/favtuts/server.log";
+        MessageDigest md = MessageDigest.getInstance("SHA-256"); // SHA, MD2, MD5, SHA-256, SHA-384...        
+        String hexSHA256 = checksum(filePath, md);
+        System.out.println(hexSHA256);
+
+        md = MessageDigest.getInstance("MD5");
+        String hexMD5 = checksum(filePath, md);
+        System.out.println(hexMD5);
+        
+        String checksumSHA256 = DigestUtils.sha256Hex(new FileInputStream(filePath));
+        System.out.println("checksumSHA256 : " + checksumSHA256);
+
+        String checksumMD5 = DigestUtils.md5Hex(new FileInputStream(filePath));
+        System.out.println("checksumMD5 : " + checksumMD5);
     }
     
     private static String checksum(String filepath, MessageDigest md) throws IOException {
 
-        // // file hashing with DigestInputStream
-        // try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
-        //     while(dis.read() != -1) ; // empty loop to clear the data
-        //     md = dis.getMessageDigest();
-        // }
-
-        // DigestInputStream is better, but you also can hash file like this.
-        try (InputStream fis = new FileInputStream(filepath)) {
-            byte[] buffer = new byte[1024];
-            int nread;
-            while ((nread = fis.read(buffer)) != -1) {
-                md.update(buffer, 0, nread);
-            }
+        // file hashing with DigestInputStream
+        try (DigestInputStream dis = new DigestInputStream(new FileInputStream(filepath), md)) {
+            while(dis.read() != -1) ; // empty loop to clear the data
+            md = dis.getMessageDigest();
         }
+
+        // // DigestInputStream is better, but you also can hash file like this.
+        // try (InputStream fis = new FileInputStream(filepath)) {
+        //     byte[] buffer = new byte[1024];
+        //     int nread;
+        //     while ((nread = fis.read(buffer)) != -1) {
+        //         md.update(buffer, 0, nread);
+        //     }
+        // }
 
         // bytes to hex
         StringBuilder result  = new StringBuilder();
---
+++ java-crypto/src/main/java/com/favtuts/crypto/hash/ListMessageDigest.java
@@ -0,0 +1,13 @@
+package com.favtuts.crypto.hash;
+
+import java.security.Security;
+import java.util.Set;
+
+public class ListMessageDigest {
+
+    public static void main(String[] args) {
+        Set<String> messageDigest = Security.getAlgorithms("MessageDigest");
+        messageDigest.forEach(x -> System.out.println(x));
+    }
+    
+}
