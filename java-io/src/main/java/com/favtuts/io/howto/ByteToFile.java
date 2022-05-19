7bcce800db8ec29102bff2d4196d123b1b4e6ad4
favtuts <favtuts@gmail.com>
2022-05-19 10:55:38 +0700

    read the file into an InputStream and uses String.format(%X) to convert each byte into a hex code

--- java-io/src/main/java/com/favtuts/io/howto/ByteToFile.java
+++ java-io/src/main/java/com/favtuts/io/howto/ByteToFile.java
@@ -1,6 +1,7 @@
 package com.favtuts.io.howto;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,9 +17,14 @@ public class ByteToFile {
         try {
 
             // tested with character data
+            String filePath = "/home/tvt/workspace/favtuts/file.txt";
 
             // file to bytes[]
-            byte[] bytes = Files.readAllBytes(Paths.get("/home/tvt/workspace/favtuts/file.txt"));
+            // byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            // byte[] bytes = readFileToBytesClassic(filePath);
+            // byte[] bytes = readFileToBytesJava7(filePath);
+            // byte[] bytes = readFileToBytesJavaNio(filePath);            
+            byte[] bytes = readFileToBytesCommonsIO(filePath);
 
             // save byte[] to a file
             writeBytesToFile("/home/tvt/workspace/favtuts/file2.txt", bytes);
@@ -29,12 +35,74 @@ public class ByteToFile {
 
 
             // tested with binary data
+            filePath = "/home/tvt/workspace/favtuts/phone.png";
+            bytes = readFileToBytesJavaNio(filePath);
+
+            // save byte[] to a file
+            writeBytesToFile("/home/tvt/workspace/favtuts/phone2.png", bytes);
+            writeBytesToFileNio("/home/tvt/workspace/favtuts/phone3.png", bytes);
+            writeBytesToFileApache("/home/tvt/workspace/favtuts/phone4.png", bytes);
             
         } catch (IOException e) {
             e.printStackTrace();
         }    
 
     }
+
+    private static byte[] readFileToBytesJavaNio(String filePath) throws IOException {       
+        byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+        return bytes;
+    }
+
+
+     // Apache Commons IO, try FileUtils.
+     private static byte[] readFileToBytesCommonsIO(String filePath) throws IOException {
+        //...
+        File file = new File(filePath);
+        byte[] bytes = FileUtils.readFileToByteArray(file);
+        return bytes;
+    }
+
+
+    // try-with-resources version
+    private static byte[] readFileToBytesJava7(String filePath) throws IOException {
+
+        File file = new File(filePath);
+        byte[] bytes = new byte[(int) file.length()];
+    
+        // funny, if can use Java 7, please uses Files.readAllBytes(path)
+        try(FileInputStream fis = new FileInputStream(file)){
+            fis.read(bytes);
+        }
+
+        return bytes;
+    }
+
+
+    // file to byte[], old and classic way, before Java 7
+    private static byte[] readFileToBytesClassic(String filePath) throws IOException {
+
+        File file = new File(filePath);
+        byte[] bytes = new byte[(int) file.length()];
+
+        FileInputStream fis = null;
+        try {
+
+            fis = new FileInputStream(file);
+
+            //read file into bytes[]
+            fis.read(bytes);
+
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+
+        return bytes;
+
+    }
+
 
     //JDK 7 - FileOutputStream + try-with-resources
     private static void writeBytesToFile(String fileOutput, byte[] bytes)
