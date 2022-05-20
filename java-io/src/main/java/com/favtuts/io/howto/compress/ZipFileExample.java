cde41cb9539fda938bc35fbd4393884b6d29a62d
favtuts <favtuts@gmail.com>
2022-05-20 12:42:17 +0700

    uses the Java 7 NIO FileSystems.newFileSystem to create a zip file and Files.copy to copy the files into the zip path

--- java-io/src/main/java/com/favtuts/io/howto/compress/ZipFileExample.java
+++ java-io/src/main/java/com/favtuts/io/howto/compress/ZipFileExample.java
@@ -3,9 +3,15 @@ package com.favtuts.io.howto.compress;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -17,10 +23,13 @@ public class ZipFileExample {
         String zipFileName = "example.zip";
 
         try {
-            zipSingleFile(source, zipFileName);
+            // zipSingleFile(source, zipFileName);
+            zipSingleFileNio(source, zipFileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Done");
     }
     
     // Zip a single file
@@ -46,5 +55,26 @@ public class ZipFileExample {
             zos.closeEntry();
         } 
 
+    }
+
+    // Zip a single file
+    public static void zipSingleFileNio (Path source, String zipFileName) throws IOException {
+
+        if (!Files.isRegularFile(source)) {
+            System.err.println("Please provide a file.");
+            return;
+        }
+
+        Map<String, String> env = new HashMap<>();
+        // Create the zip file if it doesn't exist
+        env.put("create", "true");
+
+        URI uri = URI.create("jar:file:/home/tvt/workspace/favtuts/"+zipFileName);
+        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+            Path pathInZipfile = zipfs.getPath(source.getFileName().toString());
+
+            // Copy a file into the zip file path
+            Files.copy(source, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
+        }
     }
 }
