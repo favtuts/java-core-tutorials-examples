9de3b4c2a859254283249adbd25033c287e89055
favtuts <favtuts@gmail.com>
2022-05-21 11:00:42 +0700

    assign 777 file permission during temporary file creation

--- java-io/src/main/java/com/favtuts/io/temp/CreateTempFile.java
+++ java-io/src/main/java/com/favtuts/io/temp/CreateTempFile.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+import java.nio.file.attribute.PosixFilePermission;
 
 public class CreateTempFile {
 
@@ -20,11 +23,22 @@ public class CreateTempFile {
             // Path temp = Files.createTempFile(null, ".log");
             // Path temp = Files.createTempFile("hello", "");
             // Path temp = Files.createTempFile(null, null);
-            
 
             // Create a temporary file in a specified folder
+            /*
+             * Path path = Paths.get("/home/tvt/workspace/favtuts/temp/");
+             * Path temp = Files.createTempFile(path, null, ".log");
+             */
+
+            // file permission
+            // Files.setPosixFilePermissions(tempFile,
+            // PosixFilePermissions.fromString("rwxrwxrwx"));
+
+            // Create a temporary file in a specified folder + file permission
             Path path = Paths.get("/home/tvt/workspace/favtuts/temp/");
-            Path temp = Files.createTempFile(path, null, ".log");
+            // 777
+            Set<PosixFilePermission> fp = PosixFilePermissions.fromString("rwxrwxrwx");
+            Path temp = Files.createTempFile(path, null, ".log", PosixFilePermissions.asFileAttribute(fp));
 
             System.out.println("Temp file : " + temp);
 
@@ -33,5 +47,5 @@ public class CreateTempFile {
         }
 
     }
-    
+
 }
