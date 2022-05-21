f5cf7039138021f37335f6c12050ce35f7f96f3b
favtuts <favtuts@gmail.com>
2022-05-21 11:37:50 +0700

    we can create a temporary file and substring the file path to get the temporary file location

--- java-io/src/main/java/com/favtuts/io/temp/TempFilePath.java
+++ java-io/src/main/java/com/favtuts/io/temp/TempFilePath.java
@@ -1,5 +1,11 @@
 package com.favtuts.io.temp;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class TempFilePath {
 
     public static void main(String[] args) {
@@ -7,6 +13,40 @@ public class TempFilePath {
         String tmpdir = System.getProperty("java.io.tmpdir");
         System.out.println("Temp file path: " + tmpdir);
 
+        // Java NIO
+        /*
+        try {
+            Path temp = Files.createTempFile("", ".tmp");
+
+            String absolutePath = temp.toString();
+            System.out.println("Temp file : " + absolutePath);
+
+            String separator = FileSystems.getDefault().getSeparator();
+            String tempFilePath = absolutePath
+                  .substring(0, absolutePath.lastIndexOf(separator));
+
+            System.out.println("Temp file path : " + tempFilePath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+        // Java IO
+        /*
+        try {
+            File temp = File.createTempFile("log_", ".tmp");
+            System.out.println("Temp file : " + temp.getAbsolutePath());
+
+            String absolutePath = temp.getAbsolutePath();
+            String tempFilePath = absolutePath
+                  .substring(0, absolutePath.lastIndexOf(File.separator));
+
+            System.out.println("Temp file path : " + tempFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
     }
     
 }
