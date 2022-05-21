9dfdccdc336e30165f703ad58b2de26029c75f8d
favtuts <favtuts@gmail.com>
2022-05-21 11:18:44 +0700

    assign a 777 file permission during file creation

---
+++ java-io/src/main/java/com/favtuts/io/temp/CreateFile.java
@@ -0,0 +1,37 @@
+package com.favtuts.io.temp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class CreateFile {
+
+    public static void main(String[] args) {
+
+        try {
+            Path path = Paths.get("/home/tvt/workspace/favtuts/temp/test1.log");
+
+            Set<PosixFilePermission> perms = EnumSet.of(
+                    PosixFilePermission.OWNER_READ,
+                    PosixFilePermission.OWNER_WRITE,
+                    PosixFilePermission.OWNER_EXECUTE,
+                    PosixFilePermission.GROUP_READ,
+                    PosixFilePermission.GROUP_WRITE,
+                    PosixFilePermission.GROUP_EXECUTE,
+                    PosixFilePermission.OTHERS_READ,
+                    PosixFilePermission.OTHERS_WRITE,
+                    PosixFilePermission.OTHERS_EXECUTE);
+
+            Files.createFile(path, PosixFilePermissions.asFileAttribute(perms));
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
