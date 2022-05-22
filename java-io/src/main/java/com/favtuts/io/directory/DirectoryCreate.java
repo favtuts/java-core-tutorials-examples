4c982a1c92bb3f4ed9e7753f8637d2fe3f036224
favtuts <favtuts@gmail.com>
2022-05-22 16:16:26 +0700

    use the NIO Files.createDirectory to create a directory or Files.createDirectories to create a directory including all nonexistent parent directories

---
+++ java-io/src/main/java/com/favtuts/io/directory/DirectoryCreate.java
@@ -0,0 +1,47 @@
+package com.favtuts.io.directory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryCreate {
+
+    public static void main(String[] args) {
+
+        String dir = "/home/tvt/workspace/favtuts/test2/test3/test4/";
+        //createDirectoriesNIO(dir);
+        createDirectoriesLegacy(dir);
+
+    }
+
+    public static void createDirectoriesNIO(String dir) {
+
+        try {
+
+            Path path = Paths.get(dir);
+            Files.createDirectories(path);
+            System.out.println("Directory is created!");
+
+            //Files.createDirectory(path);
+
+        } catch (IOException e) {
+            System.err.println("Failed to create directory!" + e.getMessage());
+        }
+
+    }
+
+    public static void createDirectoriesLegacy(String dir) {
+
+        File file = new File(dir);
+
+        if (file.mkdirs()) {
+            System.out.println("Directory is created!");
+        } else {
+            System.out.println("Failed to create directory!");
+        }
+
+    }
+    
+}
