8997c3b9cd26b0f613d52e0929d2078793d36fe5
favtuts <favtuts@gmail.com>
2022-06-03 15:44:48 +0700

    Java 8 IntStream examples

--- java-basic/src/main/java/com/favtuts/java8/stream/StreamIntegerExamples.java
+++ java-basic/src/main/java/com/favtuts/java8/stream/StreamIntegerExamples.java
@@ -1,13 +1,64 @@
 package com.favtuts.java8.stream;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamIntegerExamples {
     
     public static void main(String[] args) {
-        boxedIntStreamToIntegerArray();
+        //boxedIntStreamToIntegerArray();
+        //accessFirstIntValueFromIntStream();
+        //accessFilterIntValueFromIntStream();
+        convertIntStreamToIntegerArray();
+    }
+
+    static void convertIntStreamToIntegerArray() {
+        int[] num = {1, 2, 3, 4, 5};
+
+        IntStream stream = Arrays.stream(num);
+
+        // IntStream -> int[]
+        int[] ints = stream.toArray();
+
+        IntStream stream2 = Arrays.stream(num);
+
+        // IntStream -> Integer[]
+        Integer[] integers = stream2.boxed().toArray(Integer[]::new);
+    }
+
+    static void accessFilterIntValueFromIntStream() {
+        int[] num = {1, 2, 3, 4, 5};
+
+        //1. int[] -> IntStream
+        IntStream stream = Arrays.stream(num);
+
+        // 2. OptionalInt
+        OptionalInt any = stream.filter(x -> x % 2 == 0).findAny();
+
+        // 3. getAsInt()
+        int result = any.getAsInt();
+
+        System.out.println(result); // 2 or 4
+    }
+
+    static void accessFirstIntValueFromIntStream() {
+        int[] num = {1, 2, 3, 4, 5};
+
+        //1. int[] -> IntStream
+        IntStream stream = Arrays.stream(num);
+
+        // 2. OptionalInt
+        OptionalInt first = stream.findFirst();
+
+        // 3. getAsInt()
+        int result = first.getAsInt();
+
+        System.out.println(result);                                     // 1
+
+        // one line
+        System.out.println(Arrays.stream(num).findFirst().getAsInt());  // 1
     }
 
     static void boxedIntStreamToIntegerArray() {
